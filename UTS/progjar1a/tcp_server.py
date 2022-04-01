9fd3af2a45b296acf5d2428221c92a7043e9c40a
Muthia <68548653+muthiaqrrta@users.noreply.github.com>
2022-04-01 21:38:43 +0700

    connect client server

--- UTS/progjar1a/tcp_server.py
+++ UTS/progjar1a/tcp_server.py
@@ -3,12 +3,17 @@ import socket
 import logging
 import json
 import dicttoxml
+import os
+import ssl
 
 alldata = dict()
 alldata['1']=dict(nomor=1, nama="dean henderson", posisi="kiper")
 alldata['2']=dict(nomor=2, nama="luke shaw", posisi="bek kiri")
 alldata['3']=dict(nomor=3, nama="aaron wan-bissaka", posisi="bek kanan")
 alldata['4']=dict(nomor=4, nama="victor lindelof", posisi="bek tengah kanan")
+
+def versi():
+    return "versi 0.0.1"
 
 
 def proses_request(request_string):
@@ -28,6 +33,8 @@ def proses_request(request_string):
                 hasil = alldata[nomorpemain]
             except:
                 hasil = None
+        elif (command == 'versi'):
+            hasil = versi()
     except:
         hasil = None
     return hasil
@@ -41,50 +48,86 @@ def serialisasi(a):
     logging.warning(serialized)
     return serialized
 
-def run_server(server_address):
+def run_server(server_address,is_secure=False):
+    # ------------------------------ SECURE SOCKET INITIALIZATION ----
+    if is_secure == True:
+        print(os.getcwd())
+        cert_location = os.getcwd() + '/certs/'
+        socket_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        socket_context.load_cert_chain(
+            certfile=cert_location + 'domain.crt',
+            keyfile=cert_location + 'domain.key'
+        )
+    # ---------------------------------
+
     #--- INISIALISATION ---
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     # Bind the socket to the port
-    print(f"starting up on {server_address}")
+    logging.warning(f"starting up on {server_address}")
     sock.bind(server_address)
     # Listen for incoming connections
-    sock.listen(1)
+    sock.listen(1000)
 
 
     while True:
         # Wait for a connection
-        print("waiting for a connection")
-        connection, client_address = sock.accept()
-        print(f"connection from {client_address}")
+        logging.warning("waiting for a connection")
+        koneksi, client_address = sock.accept()
+        logging.warning(f"Incoming connection from {client_address}")
         # Receive the data in small chunks and retransmit it
-        while True:
-            data = connection.recv(32)
-            print(f"received {data}")
-            if data:
-                hasil = proses_request(data.decode())
-                print(hasil)
-                #hasil bisa berupa tipe dictionary
-                #harus diserialisasi dulu sebelum dikirim via network
-                # Send data
-                # some data structure may have complex structure
-                # how to send such data structure through the network ?
-                # use serialization
-                #  example : json, xml
 
-                # complex structure, nested dict
-                # all data that will be sent through network has to be encoded into bytes type"
-                # in this case, the message (type: string) will be encoded to bytes by calling encode
-
-                hasil = serialisasi(hasil)
-                hasil += "\r\n\r\n"
-                connection.sendall(hasil.encode())
+        try:
+            if is_secure == True:
+                connection = socket_context.wrap_socket(koneksi, server_side=True)
             else:
-               print(f"no more data from {client_address}")
-               break
-        # Clean up the connection
-        connection.close()
+                connection = koneksi
 
+            selesai=False
+            data_received="" #string
+            while True:
+                data = connection.recv(32)
+                logging.warning(f"received {data}")
+                if data:
+                    data_received += data.decode()
+                    if "\r\n\r\n" in data_received:
+                        selesai=True
+
+                    if (selesai==True):
+                        hasil = proses_request(data_received)
+                        logging.warning(f"hasil proses: {hasil}")
+
+                        #hasil bisa berupa tipe dictionary
+                        #harus diserialisasi dulu sebelum dikirim via network
+                        # Send data
+                        # some data structure may have complex structure
+                        # how to send such data structure through the network ?
+                        # use serialization
+                        #  example : json, xml
+
+                        # complex structure, nested dict
+                        # all data that will be sent through network has to be encoded into bytes type"
+                        # in this case, the message (type: string) will be encoded to bytes by calling encode
+
+                        hasil = serialisasi(hasil)
+                        hasil += "\r\n\r\n"
+                        connection.sendall(hasil.encode())
+                        selesai = False
+                        data_received = ""  # string
+                        break
+
+                else:
+                   logging.warning(f"no more data from {client_address}")
+                   break
+            # Clean up the connection
+        except ssl.SSLError as error_ssl:
+            logging.warning(f"SSL error: {str(error_ssl)}")
 
 if __name__=='__main__':
-    run_server(('127.0.0.1', 12000))
+    try:
+        run_server(('0.0.0.0', 12000),is_secure=False)
+    except KeyboardInterrupt:
+        logging.warning("Control-C: Program berhenti")
+        exit(0)
+    finally:
+        logging.warning("seelsai")
